2af4d90b7936b7659fda829ef0e9175a6549b0b9
Matt Morrison <matt.morrison@zapier.com>
2021-05-11 16:20:06 +1200

    Add create & delete service

--- cmd/opslevel/main.go
+++ cmd/opslevel/main.go
@@ -13,17 +13,27 @@ import (
 )
 
 var (
-	app = kingpin.New("opslevel", "OpsLevel.com command line utility")
+	app = kingpin.New("opslevel", "OpsLevel.com test client")
 
-	serviceCmd            = app.Command("service", "")
+	// Services
+	serviceCmd = app.Command("service", "")
+	// Create Service
+	serviceCreateCmd        = serviceCmd.Command("create", "create a service in OpsLevel")
+	serviceCreateCmdNameArg = serviceCreateCmd.Arg("name", "service name").Required().String()
+	// Get Service
 	serviceGetCmd         = serviceCmd.Command("get", "get a service by name / alias")
 	serviceGetCmdAliasArg = serviceGetCmd.Arg("alias", "service alias").Required().String()
-
+	// Tag Service
 	serviceTagCmd            = serviceCmd.Command("tag", "manipulate service tags")
 	serviceTagAddCmd         = serviceTagCmd.Command("add", "Add service tag")
 	serviceTagAddCmdAliasArg = serviceTagAddCmd.Arg("alias", "service alias").Required().String()
 	serviceTagAddCmdTagsArg  = serviceTagAddCmd.Arg("k=v", "key=value tag").Required().StringMap()
+	// Delete Service
+	serviceDeleteCmd          = serviceCmd.Command("delete", "delete a service in OpsLevel")
+	serviceDeleteCmdIdFlag    = serviceDeleteCmd.Flag("id", "Id of service to delete").String()
+	serviceDeleteCmdAliasFlag = serviceDeleteCmd.Flag("alias", "Alias of service to delete").String()
 
+	// Teams
 	teamCmd    = app.Command("team", "")
 	teamGetCmd = teamCmd.Command("get", "get a team by name / alias")
 	teamAlias  = teamGetCmd.Arg("alias", "team alias").Required().String()
@@ -41,6 +51,34 @@ func main() {
 	client := opslevel.NewClient(authToken)
 
 	switch cmd {
+	case serviceCreateCmd.FullCommand():
+		svc := opslevel.Service{
+			Name: *serviceCreateCmdNameArg,
+		}
+		resp, err := client.CreateService(context.Background(), svc)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		pretty.Println(resp)
+
+	case serviceDeleteCmd.FullCommand():
+		var resp *opslevel.DeleteServiceResponse
+		var err error
+
+		if serviceDeleteCmdIdFlag != nil && *serviceDeleteCmdIdFlag != "" {
+			resp, err = client.DeleteServiceById(context.Background(), *serviceDeleteCmdIdFlag)
+		} else if serviceDeleteCmdAliasFlag != nil && *serviceDeleteCmdAliasFlag != "" {
+			resp, err = client.DeleteServiceByAlias(context.Background(), *serviceDeleteCmdAliasFlag)
+		} else {
+			app.FatalUsage("--alias or --id flag must be provided")
+		}
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		pretty.Println(resp)
+
 	case serviceTagAddCmd.FullCommand():
 		for key, value := range *serviceTagAddCmdTagsArg {
 			tag, err := client.CreateTag(context.Background(), key, value, *serviceTagAddCmdAliasArg, "Service")
@@ -51,7 +89,7 @@ func main() {
 		}
 
 	case serviceGetCmd.FullCommand():
-		svc, err := client.GetService(context.Background(), *serviceGetCmdAliasArg)
+		svc, err := client.GetServiceByAlias(context.Background(), *serviceGetCmdAliasArg)
 		if err != nil {
 			log.Fatalln(err)
 		}
--- services.go
+++ services.go
@@ -5,20 +5,6 @@ import (
 	"fmt"
 )
 
-func (c *Client) GetService(ctx context.Context, alias string) (*Service, error) {
-	params := map[string]interface{}{
-		"serviceAlias": alias,
-	}
-	var res serviceResponse
-	if err := c.Do(ctx, serviceQuery, params, &res); err != nil {
-		return nil, fmt.Errorf("could not find service: %w", err)
-	}
-	if res.Account.Service == nil {
-		return nil, fmt.Errorf("no service was found by alias: %s", alias)
-	}
-	return res.Account.Service, nil
-}
-
 type Service struct {
 	Id          string
 	Name        string
@@ -43,9 +29,105 @@ type ServiceTier struct {
 	Name        string
 }
 
+func (c *Client) CreateService(ctx context.Context, service Service) (*Service, error) {
+	args := map[string]string{
+		"name":        service.Name,
+		"product":     service.Product,
+		"description": service.Description,
+		"language":    service.Language,
+		"framework":   service.Framework,
+	}
+	params := map[string]interface{}{
+		"input": args,
+	}
+	var res createServiceResponse
+	if err := c.Do(ctx, serviceCreateMutation, params, &res); err != nil {
+		return nil, err
+	}
+	// Check for application level errors
+	if err := handleGraphqlErrs(res.ServiceCreate.Errors); err != nil {
+		return nil, err
+	}
+	return res.ServiceCreate.Service, nil
+}
+
+func (c *Client) GetServiceById(ctx context.Context, id string) (*Service, error) {
+	params := map[string]interface{}{
+		"serviceId": id,
+	}
+	return c.GetService(ctx, serviceQueryById, params)
+}
+
+func (c *Client) GetServiceByAlias(ctx context.Context, alias string) (*Service, error) {
+	params := map[string]interface{}{
+		"serviceAlias": alias,
+	}
+	return c.GetService(ctx, serviceQuery, params)
+}
+
+func (c *Client) GetService(ctx context.Context, query string, params map[string]interface{}) (*Service, error) {
+	var res serviceResponse
+	if err := c.Do(ctx, serviceQuery, params, &res); err != nil {
+		return nil, fmt.Errorf("could not find service: %w", err)
+	}
+	if res.Account.Service == nil {
+		return nil, fmt.Errorf("no service was found for params: %v", params)
+	}
+	return res.Account.Service, nil
+}
+
+func (c *Client) DeleteServiceByAlias(ctx context.Context, alias string) (*DeleteServiceResponse, error) {
+	args := map[string]string{
+		"alias": alias,
+	}
+	params := map[string]interface{}{
+		"input": args,
+	}
+	res, err := c.DeleteService(ctx, serviceDeleteMutation, params)
+	return res, err
+}
+
+func (c *Client) DeleteServiceById(ctx context.Context, id string) (*DeleteServiceResponse, error) {
+	args := map[string]string{
+		"id": id,
+	}
+	params := map[string]interface{}{
+		"input": args,
+	}
+	res, err := c.DeleteService(ctx, serviceDeleteMutation, params)
+	return res, err
+}
+
+func (c *Client) DeleteService(ctx context.Context, query string, params map[string]interface{}) (*DeleteServiceResponse, error) {
+	var res DeleteServiceResponse
+	if err := c.Do(ctx, query, params, &res); err != nil {
+		return nil, err
+	}
+	// Check for application level errors
+	if err := handleGraphqlErrs(res.ServiceDelete.Errors); err != nil {
+		return nil, err
+	}
+	return &res, nil
+}
+
 type serviceResponse struct {
 	Account struct {
 		Service *Service
+	}
+}
+
+type createServiceResponse struct {
+	ServiceCreate struct {
+		Service *Service
+		Errors  []graphqlError
+	}
+}
+
+type DeleteServiceResponse struct {
+	ServiceDelete struct {
+		DeletedServiceAlias string
+		DeletedServiceId    string
+		Errors              []graphqlError
 	}
 }
 
@@ -66,6 +148,60 @@ query($serviceAlias: String) {
       tier {
         id
       }
+    }
+  }
+}
+`
+
+const serviceQueryById = `
+query($serviceAlias: String) {
+  account {
+    service(id: $serviceId){
+      id
+      name
+      aliases
+      description
+      framework
+      language
+      owner {
+        id
+      }
+      product
+      tier {
+        id
+      }
+    }
+  }
+}
+`
+
+const serviceCreateMutation = `
+mutation create($input: ServiceCreateInput!){
+  serviceCreate(input: $input){
+    service{
+      id
+      name
+      product
+      description
+      language
+      framework
+    }
+    errors {
+      path
+      message
+    }
+  }
+}
+`
+
+const serviceDeleteMutation = `
+mutation delete($input: ServiceDeleteInput!){
+  serviceDelete(input: $input){
+    deletedServiceAlias
+	deletedServiceId
+    errors {
+      path
+      message
     }
   }
 }
